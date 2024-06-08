ed92651385561aba3574bf8440f5f5d1cf000164
Pavel Stoyanov <fokervt19@gmail.com>
2024-06-08 20:42:04 +0300

    Added home page component and routing

---
+++ SuperBarber.UI/src/app/home_page/home-page/home-page.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.css'
+})
+export class HomePageComponent {
+  onSubmit(form: NgForm) {
+    
+  }
+}
